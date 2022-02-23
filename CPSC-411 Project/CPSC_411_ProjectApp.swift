0adf18a11cfbe28d02a7ef43b72459e15361c935
danielm19 <danielm19@csu.fullerton.edu>
2022-02-22 20:34:24 -0800

    Initial Commit

---
+++ CPSC-411 Project/CPSC_411_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPSC_411_ProjectApp.swift
+//  CPSC-411 Project
+//
+//  Created by Daniel Moran on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct CPSC_411_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CPSC-411 ProjectUITests/CPSC_411_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CPSC_411_ProjectUITestsLaunchTests.swift
+//  CPSC-411 ProjectUITests
+//
+//  Created by Daniel Moran on 2/22/22.
+//
+
+import XCTest
+
+class CPSC_411_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
