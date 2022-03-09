f9c4e05400232d61e3aeb77c60d999138d6689cd
nat29hant <nat29hant@csu.fullerton.edu>
2022-03-08 22:00:41 -0800

    made a button

---
+++ CPSC-411 Project/MysteryBox.swift
@@ -0,0 +1,14 @@
+//
+//  MysteryBox.swift
+//  CPSC-411 Project
+//
+//  Created by Tran, Nathan on 2/24/22.
+//
+
+import Foundation
+import UIKit
+
+class MysteryBox:UIViewController{
+    //start code here
+    
+}
