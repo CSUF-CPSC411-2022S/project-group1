0b6c710c53b6842ffe28ea6fd2d19f3a2bdb2c19
tombom66 <46984443+tombom66@users.noreply.github.com>
2022-03-10 12:46:02 -0800

    Update ContentView.swift

--- CPSC-411 Project/ContentView.swift
+++ CPSC-411 Project/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        toss()
+            
     }
 }
 
@@ -18,10 +18,64 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+    struct WelcomeScreen: View {
+    var body: some View{
+        
+            Text("Welcome to our game")
+                .font(.custom("Times New Roman", size: 40))
+                           .foregroundColor(Color.black)
+                           .padding(.bottom,30)
+                           .background(Color.white)
+                           .cornerRadius(10)
+        Image("86")
 }
-struct animation: animation{
-    
 }
-struct looping: looping{
+}
+struct toss: View{
+    @State var flipping = false
+    @State var heads = false
+    @State var intensity: Int = 0
+    @State var tailscounting: Int = 0
+    @State var headscounting: Int = 0
+    var body: some View{
+        VStack{
+            VStack{
+                Text("Heads: \(headscounting)")
+                Text("Tails: \(tailscounting)")
+            }
+            Spacer()
+            Coin(Flipping: $flipping,Heads:$heads)
+            Spacer()
+            Button("Take your Chances"){
+                FlipCoin()
+            }
+        }
+    }
+    func FlipCoin(){
+        
+    }
+    func HeadsTails(){
+        
+    }
+    func restart(){
+        
+    }
+            //WelcomeScreen()
+            
+}
+struct Coin: View {
+    @Binding var Flipping:Bool
+    @Binding var Heads: Bool
+    var body: some View{
+        ZStack{
+            Circle()
+                .foregroundColor(.blue)
+                .frame(width:100, height:100)
+            Circle()
+                .foregroundColor(.purple)
+                .frame(width: 85 , height: 85)
+        }
+    }
+     
     
 }
