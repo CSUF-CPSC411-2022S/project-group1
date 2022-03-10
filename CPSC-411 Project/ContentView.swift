63fab303e191bb8b9100ebf21e30bb5f90d9870a
nathan tran <nat29hant@csu.fullerton.edu>
2022-03-09 19:52:24 -0800

    still working on it

--- CPSC-411 Project/ContentView.swift
+++ CPSC-411 Project/ContentView.swift
@@ -9,13 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("What's in the box?!")
             .padding()
+        Image("Box")
+        Button("Open Box", action:{
+            print("")
+        }).modifier(ButtonDesign())
+    }
+}
+struct MysteryBoxView: View {
+    var body: some View {
+        VStack{
+            Text("What's in the box?!")
+                .font(.title)
+                .padding()
+            }
+        Image("Box")
+        Button("Open Box", action:{
+            print("")
+        }).modifier(ButtonDesign())
     }
 }
 
+struct RoundedBackground: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(10)
+            .background(Color.blue)
+            .cornerRadius(10)
+            .padding(10)
+    }
+}
+
+struct ButtonDesign: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .modifier(RoundedBackground())
+            .foregroundColor(Color.white)
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MysteryBoxView()
     }
 }
--- CPSC-411 Project/MysteryBox.swift
+++ CPSC-411 Project/MysteryBox.swift
@@ -2,11 +2,13 @@
 //  MysteryBox.swift
 //  CPSC-411 Project
 //
-//  Created by Tran, Nathan on 2/24/22.
+//  Created by Tran, Nathan on 3/9/22.
 //
 
 import Foundation
+import UIKit
 
-class MysteryBox{
-    //Add more code 
+class MysteryBox:UIViewController{
+    //start code here
+    
 }
