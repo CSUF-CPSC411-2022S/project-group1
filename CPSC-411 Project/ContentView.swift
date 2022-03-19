3173509927fc7f57de836ac1f3873602394fd315
nathan tran <nat29hant@csu.fullerton.edu>
2022-03-19 13:34:31 -0700

    Edit Content View to original state and remove unnecessary class

--- CPSC-411 Project/ContentView.swift
+++ CPSC-411 Project/ContentView.swift
@@ -4,38 +4,17 @@
 //
 //  Created by Daniel Moran on 2/22/22.
 //
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("What's in the box?!")
+        Text("Hello, world!")
             .padding()
-        Button("Open Box", action:{
-            print("")
-        }).modifier(ButtonDesign())
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-struct RoundedBackground: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .padding(10)
-            .background(Color.blue)
-            .cornerRadius(10)
-            .padding(10)
-    }
-}
-
-struct ButtonDesign: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .modifier(RoundedBackground())
-            .foregroundColor(Color.white)
     }
 }
