b37b043e32ac2b57e2fde7ba325670cde921822f
Nat29hant <54684954+Nat29hant@users.noreply.github.com>
2022-05-05 14:01:44 -0700

    Revert "Mystery box class"

--- CPSC-411 Project/MysteryBox.swift
+++ CPSC-411 Project/MysteryBox.swift
@@ -10,6 +10,7 @@ import SwiftUI
 //import UIKit
 class MysteryBox: ObservableObject{//:UIViewController{
     //start code here
+    //var message: String = ""
     @Published var prizes = [
         "A blank check?",
         "$5 Coupon to Target?",
@@ -18,25 +19,18 @@ class MysteryBox: ObservableObject{//:UIViewController{
         "$10 gift card for Starbucks",
     ]
     //test
-    func randomPrize()->String{
-        //let num = Int.random(in: 0...(prizes.count - 1))
-        //let index = num
-        //let temp = prizes[index]
+    /*func randomPrize()->String{
+        let num = Int.random(in: 1...prizes.count)
+        let index = num - 1
+        let temp = prizes[index]
+        //prizes.remove(at: index)
         if(prizes.count == 0){
            return "You have received every single prize"
        }
-       else{
-           let num = Int.random(in: 0...(prizes.count - 1))
-           let index = num
-           let temp = prizes[index]
-           prizes.remove(at: index)
-           return temp
-           
-       }
-        //prizes.remove(at: index)
-        //return temp
-    }
-    /*func randomPrize(){
+        prizes.remove(at: index)
+        return temp
+    }*/
+    func randomPrize(){
            
            let num: Int = Int.random(in: 1...prizes.count)
            let index = num - 1
@@ -46,44 +40,29 @@ class MysteryBox: ObservableObject{//:UIViewController{
            if(prizes.count == 0){
               print("You have received every single prize")
           }
-    }*/
-    /*func display () -> String{
-        if(prizes.isEmpty)
-        {
-            return "The Box is empty, add something to it"
-        }
-        return "The Box is not full"
-    }*/
+          
+       }
     func addPrize(_ newItem: String){
         let item: String
-        let num = Int.random(in: 1...3)
-        if(prizes.count >= 0 && num == 1)
+        if(prizes.count >= 0)
         {
             item = "$5 gift card for " + newItem
             prizes.append(item)
         }
-        else if(prizes.count >= 0 && num == 2)
-        {
-            item = "$10 gift card for " + newItem
-            prizes.append(item)
-        }
-        else if(prizes.count >= 0 && num == 3)
-        {
-            item = "You get nothing for " + newItem
-            prizes.append(item)
-        }
+        
         
     }
     func prizeList()-> String{
         return "There are " + "\(prizes.count)" + " prizes in this box"
         
     }
-    
+    /*func prizeList(){
+        print ("There are " + "\(prizes.count)" + " prizes in this box")
+        
+    }*/
+
     init (){
         
     }
 
 }
-
-    
-
